9e3a0597efaf959e99876bd52838fadb0e268df9
Tului <tgantulga@ucdavis.edu>
2021-03-29 14:14:24 -0700

    got it working wooohoooooo

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
 let odometerDiv = document.getElementById('odometer')
 odometerDiv.innerHTML = ''
 
-let firstNum = document.createElement('div')
-firstNum.classList.add('first-num')
+let inputDiv = document.getElementById('input')
+inputDiv.innerHTML = ''
+
+let inputComponent = document.createElement('input')
+inputDiv.appendChild(inputComponent)
+inputComponent.type = 'number'
 
 
-for (let i = 1; i < 10; i++) {
-  let tempDiv = document.createElement('div')
-  tempDiv.innerHTML = i
-  firstNum.appendChild(tempDiv)
+let odometerWidth = 210; // pixels
+let odometerHeight = 40; // pixels
+let odometerFontSize = 30; // pixels
+let odometerLength = Math.floor(odometerWidth / odometerFontSize) * 2
+let odometerArray = Array(odometerLength).fill(0)
+
+
+
+
+odometerDiv.style.width = `${odometerWidth}px`
+odometerDiv.style.height = `${odometerHeight}px`
+odometerDiv.style.fontSize = `${odometerFontSize}px`
+
+
+
+for (let idx = 0; idx < odometerLength; idx++) {
+  let el = document.createElement('div')
+  el.classList.add('num')
+  
+
+  for (let num = 0; num < 10; num++) {  
+    let tempEl = document.createElement('div')
+    tempEl.innerHTML = num
+    el.appendChild(tempEl)
+  }
+  // console.log(el)
+  
+  odometerDiv.appendChild(el)
 }
 
-odometerDiv.appendChild(firstNum)
+
+
+
+
+
+
+
+inputComponent.addEventListener('change', e => {
+  let number = e.target.value.split('')
+  let numLength = e.target.value.length
+  let numDivs = document.getElementsByClassName('num');
+  
+  Array.from(numDivs).forEach((numDiv, idx) => {
+    if (idx < odometerLength - numLength) {
+      numDiv.style.marginTop = `${0}%`
+    } else {
+      numDiv.style.marginTop = `${-16.1556 * parseInt(number[numLength - (odometerLength - idx)])}%`
+    }
+  })
+})
+
+
+
+
+
+
+
+
 
 
 
